25fff97b024c5edb986b06ca6001e7a5a86187bc
Stuart George <sgeorge@mega-tokyo.com>
2014-07-19 12:44:34 -0400

    Fix URI test that always failed except if ecDB was the ROOT

--- include/login/auth.php
+++ include/login/auth.php
@@ -1,13 +1,26 @@
 <?php
 	//Start session
 	session_start();
-	
+
+	$arrlp = explode("/", $_SERVER["REQUEST_URI"]);
+	if(count($arrlp) > 0)
+	{
+		$script_name = $arrlp[count($arrlp)-1];
+	}
+	else
+	{
+		$script_name = $_SERVER["REQUEST_URI"];
+	}
+
 	//Check whether the session variable SESS_MEMBER_ID is present or not
-	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
-		if ($_SERVER["REQUEST_URI"] == '/contact.php' or $_SERVER["REQUEST_URI"] == '/terms.php'){
-			
+	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == ''))
+	{
+		if ($script_name == 'contact.php' or $script_name == 'terms.php')
+		{
+
 		}
-		else {
+		else
+		{
 			header("location: login.php");
 			exit();
 		}
